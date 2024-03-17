109adf333b62e4f2bb7973192b79dcdc29f4d653
Bogat25 <68167886+Bogat25@users.noreply.github.com>
2024-03-17 12:20:40 +0100

    mar kirja a kartyakat

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
 function keresesKeresoSzora() {
   var keresoSzo = document.getElementById("keresettSzoInput").value;
   var xhr = new XMLHttpRequest();
-  xhr.open(
-    "GET",
-    "https://api.tvmaze.com/singlesearch/shows?q=" + keresoSzo,
-    true
-  );
+  xhr.open("GET", "https://api.tvmaze.com/search/shows?q=" + keresoSzo, true);
+  //console.log( "https://api.tvmaze.com/search/shows?q=" + keresoSzo);
   xhr.responseType = "json";
   xhr.onload = function () {
     if (xhr.status === 200) {
       var responseData = xhr.response;
       console.log(responseData);
-      cardCreate(responseData); //meghívom a kartya
+      responseLenghtCreate(responseData.length);
+      cardCreate(responseData); //meghívom a kartyakat
     } else {
       console.error("Eror: " + xhr.status);
     }
@@ -22,21 +20,65 @@ function keresesKeresoSzora() {
   };
   xhr.send();
 }
+function responseLenghtCreate(length) {
+  var talalatokHolderP = document.getElementById("talalatok");
+  talalatokHolderP.textContent = "Talaatok szama: " + length;
+}
 
 function cardCreate(responseData) {
   var cardHolderDiv = document.getElementById("cardHolder");
-  cardHolderDiv.innerHTML = cardHolderDiv.innerHTML + responseData.id;
-  cardHolderDiv.innerHTML =
-    cardHolderDiv.innerHTML +
-    '<div class="col-lg-3 col-md-4 col-sm-6">' +
-    '<div class="card mb-3">' +
-    '<img class="card-img-top" src="..." alt="Card image cap">' +
-    '<div class="card-body">' +
-    '<h5 class="card-title">Card 10</h5>' +
-    '<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card\'s content.</p>' +
-    '<a href="#" class="btn btn-primary">Go somewhere</a>' +
-    "</div>" +
-    "</div>" +
-    "</div>";
-    
+  cardHolderDiv.innerHTML = "";
+
+  for (var i = 0; i < responseData.length; i++) {
+    var showData = responseData[i];
+
+    // Kártya létrehozása
+    var card = document.createElement("div");
+    card.classList.add("col-lg-3", "col-md-4", "col-sm-6");
+    card.innerHTML =
+      '<div class="card mb-3">' +
+      '<img class="card-img-top img-small" src="' +
+      (showData.show.image ? showData.show.image.medium : "") +
+      '" alt="' +
+      showData.show.name +
+      '">' +
+      '<div class="card-body">' +
+      '<h5 class="card-title">' +
+      showData.show.name +
+      "</h5>" +
+      '<p class="card-text">' +
+      (showData.show.summary ? truncateSummary(showData.show.summary) : "") +
+      "</p>" +
+      '<p class="card-text"><b>IMDB ID:</b> ' +
+      showData.show.externals.imdb +
+      "</p>" +
+      '<p class="card-text"><b>Nyelv:</b> ' +
+      showData.show.language +
+      "</p>" +
+      '<p class="card-text"><b>Műfajok:</b> ' +
+      showData.show.genres.join(", ") +
+      "</p>" +
+      '<p class="card-text"><b>Premier:</b> ' +
+      showData.show.premiered +
+      "</p>" +
+      '<p class="card-text"><b>Állapot:</b> ' +
+      showData.show.status +
+      "</p>" +
+      "</div>" + // Itt zárul a card-body div
+      '<div class="card-footer">' +
+      (showData.show.summary ? truncateSummary(showData.show.summary) : "") + // Összefoglaló a kártya alján
+      "</div>" +
+      "</div>";
+    // A kártya hozzáadása a tartóhoz
+    cardHolderDiv.appendChild(card);
 }
+}
+
+  // Függvény az összefoglaló levágására, ha hosszabb, mint 200 karakter
+  function truncateSummary(summary) {
+    if (summary.length > 200) {
+      return summary.substring(0, 197) + '...'; //nem tudom hogy itt a 200 as limitbe beleszámít e a ... így a biztonság kedvéért úgy vettem hogy beleszámít
+    } else {
+      return summary;
+    }
+  }
